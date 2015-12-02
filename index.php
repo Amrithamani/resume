29036f19f2b3fe6a95353bce09493835fda9fd16
Amritha Mani <amrimani91@gmail.com>
2015-12-02 13:38:00 -0500

    creating style.css and modifying index.php

--- index.php
+++ index.php
@@ -1,1 +1,190 @@
-<h1>Hello World!</h1>
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset=UTF-8">
+<meta charset="utf-8">
+ 
+<title>Amritha Mani</title>
+ 
+<!-- Meta -->
+<meta name="description" content="A Passionate Web Programmer">
+<meta name="keywords" content="web,developer,designer,resume" />
+<meta name="author" content="Amritha Mani">
+ 
+<!-- CSS Stylesheet -->
+<link rel="stylesheet" type="text/css" href="style.css" />
+ 
+<!--[if lt IE 9]> 
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+ 
+</head>
+<body>
+ 
+<!-- Begin Wrapper -->
+<div id="wrapper">
+ 
+<!-- Begin Content Area -->
+<div id="content">
+ 
+<!-- Begin Header -->
+<header>
+ 
+<!-- Begin Contact Section -->
+<section id="contact-details">
+ 
+<!-- Begin Profile Image Section -->
+<div class="header_1">
+<img src="Amritha.jpg" width="250" height="250" alt="Amritha" />
+</div>
+<!-- End Profile Image Section -->
+ 
+<!-- Begin Profile Information Section -->
+<div class="header_2">
+ 
+<h1><span>Amritha Mani</span></h1>
+<h3>Web Developer / Designer</h3>
+ 
+<ul class="info_1">
+<li class="address">10 village brook lane, Natick, Massachusets, 01760  U.S.A.</li>
+</ul>
+ 
+<ul class="info_2">
+<li class="phone">(469) 442-9284</li>
+<li class="email"><a href="mailto:amrimani91@gmail.com">amrimani91@gmail.com</a></li>
+<li class="site_url"><a href="http://p4.amrithamani.com" title="p4.amrithamani.com">p4.amrithamani.com</a></li>
+</ul>
+ 
+</div>
+<!-- End Profile Information Section -->
+ 
+</section>
+<!-- End Contact Section -->
+ 
+</header>
+<!-- End Header -->
+ 
+<div class="clear">&nbsp;</div>
+ 
+<!-- Begin Profile Section -->
+<dl>
+<dt>Objective:</dt>
+<dd>
+ 
+<section class="summary">
+I am Amritha Mani, a passionate web programmer with coursework and project experiences developing dynamic web sites. 
+I am based in Boston, looking for web development opportunity. Have been learning web development languages and have experiences developing course projects that span all layers of dynamic web development.
+</section>
+ 
+</dd>
+</dl>
+<!-- End Profile Section -->
+
+<div class="clear">&nbsp;</div>
+
+<!-- Availability Section -->
+<dl>
+<dt>Availability:</dt>
+<dd>
+
+<section class="summary">
+ASAP with telecommuting from home.
+</section>
+</dd>
+</dl>
+<div class="clear">&nbsp;</div>
+ 
+<!-- Begin Skills Section -->
+ 
+<dl>
+<dt>Skills:</dt>
+<dd>
+ 
+ <section id="skills">
+<ul>
+<li>HTML</li>
+<li>CSS</li>
+<li>PHP</li>
+<li>JavaScript</li>
+</ul>
+ </section>
+</dd>
+</dl>
+
+<div class="clear">&nbsp;</div>
+
+ <!-- Other Skills -->
+ 
+ <dl>
+ <dt>Other Skills:</dt>
+ <dd>
+ 
+ <section id="skills">
+ <ul>
+ <li>GitHub proficiency for version control.</li> 
+ <li>Windows and UNIX scripting and knowledge of working on both OS.</li>  
+ <li>Proficient in Windows based Microsoft applications like Word, Excel, Powerpoint etc.</li>
+ <li>Basic knowledge of database table design and query.</li>
+ </ul>
+ </section>
+ </dd>
+ </dl>
+<!-- End Skills Section -->
+ 
+<div class="clear">&nbsp;</div>
+ 
+<!-- Begin Relevant Coursework Section -->
+<dl>
+<dt>Relevant Coursework:</dt>
+ 
+<dd>
+ <ul>
+ <li>Programming languages Java/C++ during undergrad – Has learnt the programming syntax and object oriented programming basics. Worked on projects for writing small algorithms.</li>
+ <li>Dynamic Web applications in Harvard extension school - https://www.extension.harvard.edu/academics/courses/dynamic-web-applications/14291 </li>
+ <li>Learnt basics of PHP development using MVC framework Laravel, web server setup, GitHub version control and testing. Final project was to develop a database-driven dynamic web application.</li>
+ <li>Course Projects source: https://github.com/Amrithamani?tab=repositories</li>
+ <li>Course web site: http://p4.amrithamani.com/</li>
+ <li>Learnt HTML, CSS and JavaScript online and applied these concepts in the coursework.</li>
+ </ul>
+ 
+</dd>
+</dl>
+<!-- End Relevant Coursework Section -->
+ 
+<div class="clear">&nbsp;</div>
+ 
+<!-- Begin Formal Education Section -->
+<dl>
+<dt>Formal Education:</dt>
+ 
+<dd>
+ 
+<section id="education">
+ 
+<p class="bus1">Maamallan Institute Of Technology</p>
+ 
+Voyalur, Tamil Nadu 602105, India
+ 
+<p class="summary">Electronics and Communication Engineering</p>
+<p class="time">2008 &#45; 2012</p>
+ 
+<p class="edu-info">
+<a href="http://www.jeppiaarmaamallan.in/" target="_blank" title="Maamallan Institute Of Technology">jeppiaarmaamallan.in</a>
+</p>
+ 
+</section>
+ 
+</dd>
+</dl>
+<!-- End Education Section -->
+ 
+ 
+</div>
+<!-- End Content -->
+ 
+</div>
+<!-- End Wrapper -->
+ 
+</body>
+</html>
